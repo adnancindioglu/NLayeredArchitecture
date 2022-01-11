c838c5d0109e32b1cbd7190283b1fa768afb1781
adnan cindioglu <adnancindioglu@hotmail.com>
2022-01-11 21:42:19 +0300

    Add project UnitOfWork Pattern

---
+++ NLayeredArchitecture.Core/UnitOfWorks/IUnitOfWork.cs
@@ -0,0 +1,17 @@
+﻿using NLayeredArchitecture.Core.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayeredArchitecture.Core.UnitOfWorks
+{
+    public interface IUnitOfWork
+    {
+        //UnitOfWork Pattern CreateiUpdate,Delete gibi işlemlerde toplu işlem olduğunda transaction gibi işlmeleri bekletip toplu işlem yapmamıza olanak tanır
+        IProductRepository Products { get; }
+        ICategoryRepository Categories { get; }
+        Task CommitAsync();
+        void Commit();
+    }
+}
